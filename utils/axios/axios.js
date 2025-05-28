2900319e4b0ace70be67e847348f9b38637cbeb7
rasinduRawishanka <63003929+Rawishanka@users.noreply.github.com>
2025-05-28 17:44:40 +0530

    axios ml service

--- utils/axios/axios.js
+++ utils/axios/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+
 const axiosServices = axios.create({ baseURL: "http://127.0.0.1:4000/api/" });
 // const axiosAuthService = axios.create({baseURL: process.env.REACT_APP_BACKEND_AUTH_API_URL});
+const axiosMLService = axios.create({ baseURL: "http://127.0.0.1:4000/api/" });
 
 axiosServices.interceptors.response.use(
     (response) => response,
@@ -10,5 +12,11 @@ axiosServices.interceptors.response.use(
     }
   );
 
+axiosMLService.interceptors.response.use(
+  (response) => response,
+  (error)=> {
+      return Promise.reject((error.response && error.response.data) || 'Wrong Services');
+    }
+)  
 export default axiosServices;
-export {axiosServices};
+export {axiosServices, axiosMLService};
